a732d88ed5d3ef765ff2b63554cd73033a8d51f3
ruonanRuby <rniu3@wisc.edu>
2019-10-27 15:01:15 -0500

    center the burger and contact info

--- burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,13 +7,14 @@ const checkoutSummary = (props) => {
     return(
         <div className= "CheckoutSummary" >
             <h1>We hope it tastes well!</h1>
-            <div style={{width:'100%', margin:'auto'}}>
-                <Burger ingredients = {props.ingredients} />
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
             </div>
             <Button
                  btnType="Danger"
                  clicked = {props.checkoutCancelled}>CANCEL</Button>
             <Button 
+                style =  {{margin: '50'}}
                 btnType="Success"
                 clicked = {props.checkoutContinued}>CONTINUE</Button>
         </div>
