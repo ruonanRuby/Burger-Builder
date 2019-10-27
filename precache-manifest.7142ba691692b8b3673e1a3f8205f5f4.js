bf3e8d1281b61a0239e2374b26fef2ffb5e14c0a
ruonanRuby <rniu3@wisc.edu>
2019-10-27 14:27:33 -0500

    Updates

---
+++ precache-manifest.7142ba691692b8b3673e1a3f8205f5f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9616344cd54e088d312d24d76b9b5f9d",
+    "url": "/Burger-Builder/index.html"
+  },
+  {
+    "revision": "17ef65ef260521ccb175",
+    "url": "/Burger-Builder/static/css/main.229b0080.chunk.css"
+  },
+  {
+    "revision": "4c515f23d9994c625c03",
+    "url": "/Burger-Builder/static/js/2.23a653ce.chunk.js"
+  },
+  {
+    "revision": "17ef65ef260521ccb175",
+    "url": "/Burger-Builder/static/js/main.a7a69a83.chunk.js"
+  },
+  {
+    "revision": "83e7ba7f53f727bbab71",
+    "url": "/Burger-Builder/static/js/runtime~main.eb88f8aa.js"
+  }
+]);
